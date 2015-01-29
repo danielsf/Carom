fa2dbaa22cbbd4e1061a73cd751b9c905d40acee
danielsf <scott.f.daniel@gmail.com>
2015-01-29 13:16:00 -0800

    added convergence to scurve example

--- src/examples/s_curve_coverage.cpp
+++ src/examples/s_curve_coverage.cpp
@@ -126,8 +126,14 @@ carom_test.set_max(max);
 printf("time to initialize\n");
 chisq.reset_timer();
 carom_test.initialize(1000);
-while(carom_test.get_called()<nsamples){
+int active_nodes=1;
+while(carom_test.get_called()<nsamples && active_nodes>0){
     carom_test.search();
+    active_nodes=carom_test.active_nodes();
+    if(active_nodes==0){
+        carom_test.simplex_search();
+        active_nodes=carom_test.active_nodes();
+    }
 }
 
 /*
