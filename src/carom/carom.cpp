12770d830eaf5aabd67b3c33f77f8b561d782689
danielsf <scott.f.daniel@gmail.com>
2015-03-27 16:45:45 -0700

    print chimin to timing file

--- src/carom/carom.cpp
+++ src/carom/carom.cpp
@@ -101,8 +101,9 @@ void carom::write_pts(){
     fclose(output);
     
     output=fopen(_timingname,"a");
-    fprintf(output,"%d %e %e -- %e %e -- overhead %e -- %d %d %d -- %d %d -- ",
+    fprintf(output,"%d min %e -- timing -- %e %e -- %e %e -- overhead %e -- %d %d %d -- %d %d -- ",
         _chifn.get_called(),
+        _chifn.chimin(),
         double(time(NULL))-_time_started,
         (double(time(NULL))-_time_started)/double(_chifn.get_called()),
         _chifn.get_time_spent(),
