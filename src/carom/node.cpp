b363b1e3965acb534017d92d2e0812a38b76af7f
danielsf <scott.f.daniel@gmail.com>
2015-04-21 09:32:21 -0700

    reinstate the "if hypervolume changes by 10% or more on final find_bases, resume searching" rule

--- src/carom/node.cpp
+++ src/carom/node.cpp
@@ -2687,8 +2687,22 @@ void node::search(){
         _active=0;
     }
 
+    double maxDeltaV=0.1;
+
     if(_active==0){
+        volume0=volume();
+        projectedVolume0=projected_volume();
+        
         find_bases();
+        
+        volume1=volume();
+        projectedVolume1=projected_volume();
+        
+        if(fabs(volume0-volume1)>maxDeltaV*volume0 ||
+           fabs(projectedVolume0-projectedVolume1)>maxDeltaV*projectedVolume0){
+       
+           initialize_ricochet();
+       }
     }
     
 
