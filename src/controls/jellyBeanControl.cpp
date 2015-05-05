5e45296e34e6b0b0d6cbad3c46f4d8da35f5f23b
danielsf <scott.f.daniel@gmail.com>
2015-05-04 17:04:18 -0700

    jellyBeanControl works

--- src/controls/jellyBeanControl.cpp
+++ src/controls/jellyBeanControl.cpp
@@ -8,15 +8,16 @@ int dim=5;
 
 double angularWidth,radiusOfCurvature;
 
-angularWidth=2.0;
-radiusOfCurvature=2.0;
+angularWidth=1.0;
+radiusOfCurvature=20.0;
 
-jellyBean chisq(dim,angularWidth,radiusOfCurvature;);
+jellyBean chisq(dim,angularWidth,radiusOfCurvature);
 
-array_1d<double> true_center,widths,curvature_center;
+array_1d<double> true_center,widths,curvature_center,radial_direction;
 true_center.set_name("true_center");
 curvature_center.set_name("curvature_center");
 widths.set_name("widths");
+radial_direction.set_name("radial_direction");
 
 int ix,iy;
 for(ix=0;ix<dim;ix++){
@@ -24,6 +25,11 @@ for(ix=0;ix<dim;ix++){
     widths.set(ix,chisq.get_width(0,ix));
 }
 chisq.get_curvature_center(curvature_center);
+
+for(ix=0;ix<2;ix++){
+    radial_direction.set(ix,true_center.get_data(ix)-curvature_center.get_data(ix));
+}
+radial_direction.normalize();
 
 printf("radial width %e\n",widths.get_data(1));
 
@@ -54,6 +60,10 @@ int i,j,dd,lastdex;
 dd=100;
 
 double xval,yval,mu,dx,dy;
+double theta,volume,dtheta,dr,rval;
+double xmin,xmax,ymin,ymax;
+double globalXmin,globalXmax,globalYmin,globalYmax;
+double globalDx,globalDy,xMargin;
 int rowct;
 
 min.set_dim(2);
@@ -90,19 +100,101 @@ for(ix=0;ix<dim;ix++){
            trial.set(i,true_center.get_data(i));
        }
        
+
        dx=4.0*widths.get_data(ix)/double(dd/2);
        dy=4.0*widths.get_data(iy)/double(dd/2);
        
+       xmin=true_center.get_data(ix)-(dd/2)*dx;
+       xmax=true_center.get_data(ix)+(dd/2)*dx;
+       
+       ymin=true_center.get_data(iy)-(dd/2)*dy;
+       ymax=true_center.get_data(iy)+(dd/2)*dy;
+
+       if(ix==0 && iy==1){
+           dx=widths.get_data(1)*4.0/double(dd/2);
+           dy=widths.get_data(1)*4.0/double(dd/2);
+           
+           xmin=2.0*exception_value;
+           xmax=-2.0*exception_value;
+           ymin=2.0*exception_value;
+           ymax=-2.0*exception_value;
+           
+           for(theta=-4.0*angularWidth;theta<4.0*angularWidth;theta+=0.01){
+               for(rval=radiusOfCurvature-(dd/2)*dx;rval<radiusOfCurvature+(dd/2)*dx;rval+=dx){
+                   xval=curvature_center.get_data(0)+rval*(
+                        radial_direction.get_data(0)*cos(theta)-
+                        radial_direction.get_data(1)*sin(theta));
+               
+                   yval=curvature_center.get_data(1)+rval*(
+                        radial_direction.get_data(0)*sin(theta)+
+                        radial_direction.get_data(1)*cos(theta));
+               
+                   if(xval<xmin)xmin=xval;
+                   if(xval>xmax)xmax=xval;
+                   if(yval<ymin)ymin=yval;
+                   if(yval>ymax)ymax=yval;
+               }
+           }
+       
+           dy=(ymax-ymin)/double(400);
+           dx=(xmax-xmin)/double(400);
+       
+           globalXmin=xmin;
+           globalXmax=xmax;
+           globalYmin=ymin;
+           globalYmax=ymax;
+           
+           globalDx=dx;
+           globalDy=dy;
+       
+       }
+       else if(ix==0){
+           xmin=globalXmin;
+           xmax=globalXmax;
+           dx=globalDx;
+       }
+       else if(ix==1){
+           xmin=globalYmin;
+           xmax=globalYmax;
+           dx=globalDy;
+       }
+       
        rowct=0;
        total=0.0;
-       for(i=-dd/2;i<dd/2;i++){
-           xval=true_center.get_data(ix)+i*dx;
+       
+       
+       printf("\nx %e %e %e y %e %e %e\n",xmin,xmax,dx,ymin,ymax,dy);
+
+       for(xval=xmin;xval<xmax;xval+=dx){
            trial.set(ix,xval);
-           for(j=-dd/2;j<dd/2;j++){
-               yval=true_center.get_data(iy)+j*dy;
+           for(yval=ymin;yval<ymax;yval+=dy){
                trial.set(iy,yval);
                
-               mu=chisq(trial);
+               
+               if(ix>=2 || (ix==0 && iy==1)){
+                   mu=chisq(trial);
+               }
+               else if(ix==0){
+                   mu=0.0;
+                   for(xMargin=globalYmin;xMargin<globalYmax;xMargin+=globalDy){
+                       trial.set(1,xMargin);
+                       mu+=exp(-0.5*chisq(trial))*globalDy;
+                   }
+                   
+                   mu=-2.0*log(mu);
+                   trial.set(1,true_center.get_data(1));
+               }
+               else if(ix==1){
+                   mu=0.0;
+                   for(xMargin=globalXmin;xMargin<globalXmax;xMargin+=globalDx){
+                       trial.set(0,xMargin);
+                       mu+=exp(-0.5*chisq(trial))*globalDx;
+                   }
+                   
+                   mu=-2.0*log(mu);
+                   trial.set(0,true_center.get_data(0));
+               }
+               
                total+=exp(-0.5*mu);
                chival.add(mu);
                xx.set(rowct,0,xval);
@@ -112,6 +204,7 @@ for(ix=0;ix<dim;ix++){
            
            }
        }
+
        printf("rowct %d\n",rowct);
        sort_and_check(chival,chival_sorted,dexes);
        sum=0.0;
