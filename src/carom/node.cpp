a2d8a66f3b106bab75cab15cf5f6b2fd8bd6c9ad
danielsf <scott.f.daniel@gmail.com>
2015-07-23 11:52:26 -0700

    resume ricochet if projected volume grows by too much

--- src/carom/node.cpp
+++ src/carom/node.cpp
@@ -2848,7 +2848,9 @@ void node::search(){
         volume1=volume();
         projectedVolume1=projected_volume();
         
-        if(fabs(volume0-volume1)>maxDeltaV*volume0){
+        if(fabs(volume0-volume1)>maxDeltaV*volume0 ||
+           fabs(projectedVolume0-projectedVolume1)>maxDeltaV*projectedVolume0){
+
            initialize_ricochet();
            _active=1;
        }
