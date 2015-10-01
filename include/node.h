99b6969f9694afcc65ae5e6584691f50ca6eeab3
danielsf <scott.f.daniel@gmail.com>
2015-09-30 18:50:57 -0700

    removed t_kick

--- include/node.h
+++ include/node.h
@@ -161,7 +161,6 @@ private:
     int kick_particle(int, array_1d<double>&);
     int step_kick(int, double, array_1d<double>&);
     int mcmc_kick(int, int*, array_1d<double>&, int);
-    int t_kick(int,array_1d<double>&);
     int smart_step_kick(int, double, array_1d<double>&);
     int originate_particle_compass(array_1d<double>&);
     int originate_particle_shooting(array_1d<double>&);
