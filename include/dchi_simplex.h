12140ff485a9ba34c53be3f6c51d19a4eefdc186
danielsf <scott.f.daniel@gmail.com>
2015-10-15 15:43:53 -0700

    wrote dchi_multimodal_simplex class

--- include/dchi_simplex.h
+++ include/dchi_simplex.h
@@ -29,4 +29,15 @@ class dchi_boundary_simplex : public dchi_simplex_base{
         virtual double operator()(array_1d<double>&);
 };
 
+
+class dchi_multimodal_simplex : public dchi_simplex_base{
+
+    public:
+        dchi_multimodal_simplex(chisq_wrapper*, array_1d<int>&);
+        ~dchi_multimodal_simplex(){};
+
+        void set_norm(array_1d<double>&);
+        virtual double operator()(array_1d<double>&);
+};
+
 #endif
--- src/carom/dchi_simplex.cpp
+++ src/carom/dchi_simplex.cpp
@@ -79,3 +79,40 @@ double dchi_boundary_simplex::operator()(array_1d<double> &pt){
     return dmu-exp(-0.1*dmu/delta)*delta*distance*2.0;
 }
 
+dchi_multimodal_simplex::dchi_multimodal_simplex(chisq_wrapper *cc, array_1d<int> &aa) :
+                         dchi_simplex_base(cc, aa){}
+
+void dchi_multimodal_simplex::set_norm(array_1d<double> &nn){
+
+    if(nn.get_dim()!=_norm.get_dim()){
+        printf("WARNING cannot set norm in dchi_multimodal_simplex\n");
+        printf("input dim %d need %d\n",nn.get_dim(),_norm.get_dim());
+        exit(1);
+    }
+
+    int i;
+    for(i=0;i<_norm.get_dim();i++){
+        _norm.set(i,nn.get_data(i));
+    }
+}
+
+double dchi_multimodal_simplex::operator()(array_1d<double> &pt){
+
+    double mu;
+    int i_found;
+    _chisq->evaluate(pt,&mu,&i_found);
+
+    double delta=_chisq->target()-_chisq->chimin();
+    double dmu=fabs(mu-_chisq->chimin());
+
+    double exp_term=exp(-0.1*dmu/delta);
+    double distance_term;
+    if(exp_term>1.0e-5){
+        distance_term=1.0/(associate_distance(pt)+1.0e-6);
+    }
+    else{
+        distance_term=0.0;
+    }
+
+    return mu+exp_term*distance_term;
+}
