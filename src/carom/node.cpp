e60121a3718319186461ce0246a0e483a87a4047
danielsf <scott.f.daniel@gmail.com>
2015-10-20 13:47:55 -0700

    finite growth criterion

--- src/carom/node.cpp
+++ src/carom/node.cpp
@@ -3461,7 +3461,7 @@ void node::set_particle(int ip, int ii, array_1d<double> &dir){
     }
 
     double v1=volume();
-    if(v1>_v0 || ip>=_ricochet_strikes.get_dim()){
+    if(v1>1.001*_v0 || ip>=_ricochet_strikes.get_dim()){
         _ricochet_strikes.set(ip,0);
 
         _v0=v1;
