7e31a871ae433fd66afa45ab54811d399596e63c
danielsf <scott.f.daniel@gmail.com>
2015-12-30 19:01:22 -0800

    longer test likelihood

--- include/exampleLikelihoods.h
+++ include/exampleLikelihoods.h
@@ -10,7 +10,8 @@ class gaussianJellyBean : public jellyBeanData{
         jellyBeanData(4,1,1.0,100,0.4,0.4,0.02,20.0){
             _widths.set(0,2,0.1);
             _widths.set(0,3,0.1);
-            _widths.set(0,0,20.0);
+            _widths.set(0,1,1.0);
+            _widths.set(0,0,40.0);
         }
 
 };
