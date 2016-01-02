0d0b12feb9decf2de8069ae0a70ff88919c1dfbf
danielsf <scott.f.daniel@gmail.com>
2016-01-01 17:00:03 -0800

    reorder example _widths

--- include/exampleLikelihoods.h
+++ include/exampleLikelihoods.h
@@ -8,10 +8,10 @@ class gaussianJellyBean : public jellyBeanData{
     public:
         gaussianJellyBean() :
         jellyBeanData(4,1,1.0,100,0.4){
-            _widths.set(0,2,0.1);
-            _widths.set(0,3,0.1);
-            _widths.set(0,1,1.0);
             _widths.set(0,0,40.0);
+            _widths.set(0,1,1.0);
+            _widths.set(0,2,1.0);
+            _widths.set(0,3,3.0);
         }
 
 };
