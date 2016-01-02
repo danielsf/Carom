3b5db767e6a94826e9a5bdbd5939646be6813ba1
danielsf <scott.f.daniel@gmail.com>
2016-01-01 18:00:41 -0800

    good set of constructor params

--- include/exampleLikelihoods.h
+++ include/exampleLikelihoods.h
@@ -8,7 +8,7 @@ class gaussianJellyBean : public jellyBeanData{
     public:
         gaussianJellyBean() :
         jellyBeanData(4,1,1.0,100,0.4){
-            _widths.set(0,0,40.0);
+            _widths.set(0,0,250.0);
             _widths.set(0,1,1.0);
             _widths.set(0,2,1.0);
             _widths.set(0,3,3.0);
