1c5a40147ecc2b523538c4dc3874dd125204c3e1
danielsf <scott.f.daniel@gmail.com>
2016-02-03 22:41:09 -0800

    test bias and var

--- src/tests/test_gp.cpp
+++ src/tests/test_gp.cpp
@@ -46,31 +46,48 @@ int main(){
     gg.optimize(test_set,test_ff);
     printf("\nnow lin\n");
     gg_lin.optimize(test_set,test_ff);
-    exit(1);
 
-    double guess,lin_guess,truth;
+    double gp_guess,lin_guess,truth;
 
     int lin_better=0,gp_better=0;
+    double lin_bias=0.0,gp_bias=0.0;
+    double lin_norm_bias=0.0,gp_norm_bias=0.0;
+    double lin_sq=0.0,gp_sq=0.0;
+
 
     for(i=0;i<10000;i++){
         for(j=0;j<dim;j++){
             trial.set(j,min.get_data(j)+dice.doub()*(max.get_data(j)-min.get_data(j)));
         }
         truth=chifn(trial);
-        guess=exp(gg(trial));
-        lin_guess=exp(gg_lin(trial));
-        gg.add_pt(trial,log(truth));
-        gg_lin.add_pt(trial,log(truth));
-        if(fabs(truth-guess)<fabs(truth-lin_guess)){
+        gp_guess=gg(trial);
+        lin_guess=gg_lin(trial);
+        //gg.add_pt(trial,truth);
+        //gg_lin.add_pt(trial,truth);
+        if(fabs(truth-gp_guess)<fabs(truth-lin_guess)){
             gp_better++;
         }
         else{
             lin_better++;
         }
-        printf("truth %e guess %e lin_guess %e\n",truth,guess,lin_guess);
-        
+        lin_bias+=lin_guess-truth;
+        gp_bias+=gp_guess-truth;
+        lin_norm_bias+=(lin_guess-truth)/fabs(truth);
+        gp_norm_bias+=(gp_guess-truth)/fabs(truth);
+        lin_sq+=power(lin_guess-truth,2);
+        gp_sq+=power(gp_guess-truth,2);
     }
 
+    gp_bias=gp_bias/double(i);
+    lin_bias=lin_bias/double(i);
+    gp_norm_bias=gp_norm_bias/double(i);
+    lin_norm_bias=lin_norm_bias/double(i);
+    gp_sq=gp_sq/double(i);
+    lin_sq=gp_sq/double(i);
+
     printf("gp better %d lin better %d\n",gp_better, lin_better);
+    printf("gp bias %e lin bias %e\n",gp_bias,lin_bias);
+    printf("gp norm bias %e lin norm bias %e\n",gp_norm_bias, lin_norm_bias);
+    printf("gp var %e lin var %e\n",gp_sq-gp_bias*gp_bias, lin_sq-lin_bias*lin_bias);
 
 }
