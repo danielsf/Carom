cb6cf56d92ead0106803ef700db4b9cb42ba0c33
danielsf <scott.f.daniel@gmail.com>
2016-03-04 15:42:10 -0800

    use ff median rather than ff min in gp_lin

--- src/utils/gp_lin.cpp
+++ src/utils/gp_lin.cpp
@@ -57,12 +57,16 @@ double gp_lin::operator()(array_1d<double> &pt){
     int local_mindex;
     double mu_min;
 
+    array_1d<double> raw_ff,raw_ff_sorted;
+    array_1d<int> raw_ff_dex;
+
     for(i=0;i<local_dex.get_dim();i++){
-        if(i==0 || _fn->get_data(local_dex.get_data(i))<mu_min){
-            mu_min=_fn->get_data(local_dex.get_data(i));
-            local_mindex=local_dex.get_data(i);
-        }
+        raw_ff.add(_fn->get_data(local_dex.get_data(i)));
+        raw_ff_dex.add(local_dex.get_data(i));
     }
+
+    sort_and_check(raw_ff, raw_ff_sorted, raw_ff_dex);
+    local_mindex=raw_ff_dex.get_data(raw_ff.get_dim()/2);
 
     array_1d<double> dir,mu,trial;
     dir.set_name("operator_dir");
