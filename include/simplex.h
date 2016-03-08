c173dcf01e96e73e79d989da685cbd317a0ba708
danielsf <scott.f.daniel@gmail.com>
2016-03-08 10:59:50 -0800

    whitespace in simplex.h

--- include/simplex.h
+++ include/simplex.h
@@ -12,7 +12,7 @@ public:
     simplex_minimizer();
     simplex_minimizer(double,double,double);
     ~simplex_minimizer();
-    
+
     void set_chisquared(function_wrapper*);
     void set_cost(function_wrapper*);
     void set_dice(Ran*);
@@ -23,7 +23,7 @@ public:
     void unfreeze_temp();
     void get_minpt(array_1d<double>&);
     void is_a_model();
-    
+
     /*
     the array_2d will be the input array of points;
     the array_1d will be the output minimum point
@@ -32,24 +32,24 @@ public:
     double get_minimum();
 
 private:
-    
+
     double evaluate(array_1d<double>&);
     double evaluate_cost(array_1d<double>&);
     void cool_off();
-    
+
     void gradient_minimizer();
     void gradient_cloud();
     void calculate_gradient(array_1d<double>&, array_1d<double>&);
     double get_dx(int);
     void expand();
-    
+
     void find_il();
     void paranoia();
-    
+
     void initialize();
-    
+
     void is_it_safe(char*);
-    
+
     double _temp,_min_ff,_true_min_ff,_fstar,_fstarstar,_min_temp;
     double _alpha,_beta,_gamma;
     int _il,_ih,_called_cost,_freeze_temp,_use_gradient;
@@ -61,7 +61,7 @@ private:
     array_1d<double> _last_improved_ff;
     array_1d<double> _min,_max;
     array_2d<double> _pts,_last_improved_pts;
-    
+
     Ran *_dice;
     function_wrapper *_chisquared, *_cost;
     
