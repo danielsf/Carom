02c0a693a757e75786aa3821d5cb6868bc04bb35
danielsf <scott.f.daniel@gmail.com>
2016-03-08 15:16:55 -0800

    use multimodal simplex in dalex

--- src/carom/dalex.cpp
+++ src/carom/dalex.cpp
@@ -1387,7 +1387,8 @@ void dalex::simplex_boundary_search(){
 
     //dchi_boundary_simplex_gp dchifn(_chifn,&interpolator,_good_points);
     //dchi_boundary_simplex dchifn(_chifn,_good_points);
-    dchi_interior_simplex dchifn(_chifn,_good_points);
+    //dchi_interior_simplex dchifn(_chifn,_good_points);
+    dchi_multimodal_simplex dchifn(_chifn,_good_points);
 
     simplex_minimizer ffmin;
     ffmin.set_chisquared(&dchifn);
@@ -1582,7 +1583,7 @@ void dalex::explore(){
     interpolator.set_ell_factor(1.0);
 
     //dchi_boundary_simplex_gp dchifn(_chifn, &interpolator, _good_points);
-    dchi_boundary_simplex dchifn(_chifn, _good_points);
+    dchi_multimodal_simplex dchifn(_chifn, _good_points);
 
     double rr=_explorer_step/sqrt(double(_chifn->get_dim()));
 
