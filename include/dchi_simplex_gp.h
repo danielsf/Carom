f323e4105662d34f469c1801317df7c3baa38594
danielsf <scott.f.daniel@gmail.com>
2016-03-15 15:35:20 -0700

    dchi_simplex_gp does a simple predicted-mu minimization

--- include/dchi_simplex_gp.h
+++ include/dchi_simplex_gp.h
@@ -4,17 +4,25 @@
 #include "gp_lin.h"
 #include "dchi_simplex.h"
 
-class dchi_boundary_simplex_gp : public dchi_boundary_simplex{
+class dchi_boundary_simplex_gp : public function_wrapper{
 
     public:
-        dchi_boundary_simplex_gp(chisq_wrapper*, gp_lin*, array_1d<int>&);
+        dchi_boundary_simplex_gp(chisq_wrapper *cc, gp_lin *gg){
+            _chifn=cc;
+            _interpolator=gg;
+            _called=0;
+        }
         ~dchi_boundary_simplex_gp(){};
 
         virtual double operator()(array_1d<double>&);
+        virtual int get_called(){
+            return _called;
+        }
 
     private:
+        chisq_wrapper *_chifn;
         gp_lin *_interpolator;
-        int _last_real_call;
+        int _called;
 
 };
 
--- src/carom/dchi_simplex_gp.cpp
+++ src/carom/dchi_simplex_gp.cpp
@@ -1,39 +1,14 @@
 #include "dchi_simplex_gp.h"
 
-dchi_boundary_simplex_gp::dchi_boundary_simplex_gp(chisq_wrapper *cc,
-                            gp_lin *gg, array_1d<int> &ii) :
-                          dchi_boundary_simplex(cc, ii){
-
-    _interpolator=gg;
-    _last_real_call=0;
-
-}
 
 double dchi_boundary_simplex_gp::operator()(array_1d<double> &pt){
-    double distance=associate_distance(pt);
-    double mu;
-    int i_found;
-
-    mu=_interpolator[0](pt);
-    if(_called>0 && _called-_last_real_call>50){
-        mu=_chifn[0](pt);
-        _last_real_call=_called;
-    }
-
-    double dmu;
-
-    dmu=fabs(_chifn->target()-mu);
-
-    double delta=_chifn->target()-_chifn->chimin();
-
-    double exp_term;
-    if(_chifn->target()<mu){
-        exp_term=exp(-0.1*fabs(dmu)/delta);
-    }
-    else{
-        exp_term=1.0;
-    }
-
     _called++;
-    return dmu-exp_term*delta*distance*2.0;
+    double mu=_chifn[0](pt);
+    if(mu<_chifn->target()){
+        return mu;
+    }
+
+    double predicted=_interpolator[0](pt);
+    return mu-predicted;
+
 }
