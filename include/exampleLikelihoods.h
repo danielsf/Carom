3fc376e4d0073d081e3b323e31667db1f0fbead0
danielsf <scott.f.daniel@gmail.com>
2016-06-30 17:07:31 -0700

    more integrable example chisq function

--- include/exampleLikelihoods.h
+++ include/exampleLikelihoods.h
@@ -9,10 +9,31 @@ class integrableJellyBean : public jellyBeanData{
         integrableJellyBean() :
         jellyBeanData(4,1,1.0,100,0.4){
             _widths.set(0,0,100.0);
-            _widths.set(0,1,1.0);
-            _widths.set(0,2,1.0);
+            _widths.set(0,1,2.0);
+            _widths.set(0,2,10.0);
             _widths.set(0,3,4.0);
+
             _parabola_curvature=4.0;
+
+            _bases.set(0,0,0.3);
+            _bases.set(0,1,sqrt(1.0-0.3*0.3));
+            _bases.set(0,2,0.0);
+            _bases.set(0,3,0.0);
+
+            _bases.set(1,0,-1.0*_bases.get_data(0,1));
+            _bases.set(1,1,_bases.get_data(0,0));
+            _bases.set(1,2,0.0);
+            _bases.set(1,3,0.0);
+
+            _bases.set(2,0,0.0);
+            _bases.set(2,1,0.0);
+            _bases.set(2,2,0.4);
+            _bases.set(2,3,sqrt(1.0-0.4*0.4));
+
+            _bases.set(3,0,0.0);
+            _bases.set(3,1,0.0);
+            _bases.set(3,2,_bases.get_data(2,3));
+            _bases.set(3,3,-1.0*_bases.get_data(2,2));
         }
 };
 
