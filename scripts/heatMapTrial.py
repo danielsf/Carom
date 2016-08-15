a01f91e3d6125f8eb1fa7f0529e3ae7d50676d7d
danielsf <scott.f.daniel@gmail.com>
2016-08-14 18:21:22 -0700

    use color mesh; logarithmic color part (for heatmap)

--- scripts/heatMapTrial.py
+++ scripts/heatMapTrial.py
@@ -2,6 +2,7 @@ from __future__ import with_statement
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+from matplotlib.colors import LogNorm
 import os
 
 if __name__ == "__main__":
@@ -13,23 +14,55 @@ if __name__ == "__main__":
     iy_list = (1, 2, 3, 2, 3, 3)
 
     data = {}
-    
+
     for ix, iy in zip(ix_list, iy_list):
         name = 'integrable_extended_0.95_%d_%d_heatmap.txt' % (ix, iy)
         input_name = os.path.join(scratch_dir, name)
         data['%d_%d' % (ix, iy)] = np.genfromtxt(input_name).transpose()
-        
+
 
     plt.figure(figsize=(30, 30))
     for ifig, (ix, iy) in enumerate(zip(ix_list, iy_list)):
+
         tag = '%d_%d' % (ix, iy)
         plt.subplot(3, 2, ifig+1)
-        indices = np.where(data[tag][2]<20.0)
-        plt.scatter(data[tag][0][indices], data[tag][1][indices],
-                    c=data[tag][2][indices],
-                    #c=np.exp(-0.5*data[tag][2]/data[tag][2].max()),
-                    cmap=plt.cm.gist_ncar,
-                    edgecolor='')
+        indices = np.where(data[tag][2]<50.0)
+
+        xx = data[tag][0][indices]
+        yy = data[tag][1][indices]
+        cc = data[tag][2][indices]
+
+        x_min = xx.min()
+        x_max = xx.max()
+        y_min = yy.min()
+        y_max = yy.max()
+
+        xx_sorted = np.sort(xx)
+        dx_raw = np.unique(np.diff(xx_sorted))
+        dx = dx_raw[np.where(dx_raw>1.0e-10)].min()
+
+        yy_sorted = np.sort(yy)
+        dy_raw = np.unique(np.diff(yy_sorted))
+        dy = dy_raw[np.where(dy_raw>1.0e-10)].min()
+
+        xx_grid, yy_grid = np.mgrid[slice(x_min, x_max+dx, dx),
+                                    slice(y_min, y_max+dy, dy)]
+
+        cc_grid = np.ones(xx_grid.shape)*(-99.0)
+
+        for ii in range(len(xx)):
+            ix = int((xx[ii]-x_min)/dx)
+            iy = int((yy[ii]-y_min)/dy)
+            cc_grid[ix][iy] = cc[ii]
+
+        cc_masked = np.ma.masked_values(cc_grid, -99.0)
+
+
+        plt.pcolormesh(xx_grid, yy_grid, cc_masked,
+                       cmap=plt.cm.gist_ncar,
+                       edgecolor='',
+                       norm=LogNorm(vmin=cc.min(), vmax=cc.max()))
+
         ticks=np.linspace(0.0,30.0,5.0,endpoint=True)
         #plt.contour(data[tag][0], data[tag][1], data[tag][2],ticks)
         xmax=data[tag][0][indices].max()
