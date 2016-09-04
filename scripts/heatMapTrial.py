c5cdb4a0f43965d350cb03b3753d536136a20e1b
danielsf <scott.f.daniel@gmail.com>
2016-09-03 17:00:10 -0700

    update heatmap script

--- scripts/heatMapTrial.py
+++ scripts/heatMapTrial.py
@@ -1,5 +1,8 @@
 from __future__ import with_statement
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 from matplotlib.colors import LogNorm
@@ -22,15 +25,15 @@ if __name__ == "__main__":
 
 
     plt.figure(figsize=(30, 30))
-    for ifig, (ix, iy) in enumerate(zip(ix_list, iy_list)):
+    for ifig, (xdex, ydex) in enumerate(zip(ix_list, iy_list)):
 
-        tag = '%d_%d' % (ix, iy)
+        tag = '%d_%d' % (xdex, ydex)
         plt.subplot(3, 2, ifig+1)
-        indices = np.where(data[tag][2]<50.0)
+        indices = np.where(data[tag][3]>1.0e-6)
 
         xx = data[tag][0][indices]
         yy = data[tag][1][indices]
-        cc = data[tag][2][indices]
+        cc = data[tag][3][indices]
 
         x_min = xx.min()
         x_max = xx.max()
@@ -62,6 +65,8 @@ if __name__ == "__main__":
                        cmap=plt.cm.gist_ncar,
                        edgecolor='',
                        norm=LogNorm(vmin=cc.min(), vmax=cc.max()))
+        cbar = plt.colorbar()
+        cbar.ax.tick_params(labelsize=40)
 
         ticks=np.linspace(0.0,30.0,5.0,endpoint=True)
         #plt.contour(data[tag][0], data[tag][1], data[tag][2],ticks)
@@ -71,7 +76,11 @@ if __name__ == "__main__":
         ymin=data[tag][1][indices].min()
         dx=(xmax-xmin)*0.1
         dy=(ymax-ymin)*0.1
-        plt.text(xmax-2*dx, ymax-1*dy, tag, fontsize=30)
-        plt.colorbar()
+        #plt.text(xmax-2*dx, ymax-1*dy, tag, fontsize=30)
+        plt.xlabel("$\\theta_%d$" % xdex, fontsize=40)
+        plt.ylabel("$\\theta_%d$" % ydex, fontsize=40)
+        plt.xticks(fontsize=40)
+        plt.yticks(fontsize=40)
 
-    plt.savefig(os.path.join(scratch_dir, 'heat_map.eps'))
+    plt.tight_layout()
+    plt.savefig(os.path.join(scratch_dir, 'heat_map.png'))
