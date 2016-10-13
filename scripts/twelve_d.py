2267d5aba0501b518aea485a3377a15269983d16
danielsf <scott.f.daniel@gmail.com>
2016-10-13 13:45:26 -0700

    playing with limit_list

--- scripts/twelve_d.py
+++ scripts/twelve_d.py
@@ -187,7 +187,7 @@ def boundary_from_multinest_projection(file_name, dim, ix, iy, data=None):
 if __name__ == "__main__":
 
     seed_list = [626, 694, 762, 1068, 6475]
-    limit_list = [500000, 400000, 400000, 500000, 500000]
+    limit_list = [400000, 400000, 700000, 700000, 400000]
     title_list = ['(a)', '(b)', '(c)', '(d)', '(e)']
     color_list = ['r', 'y', 'g', 'c']
 
