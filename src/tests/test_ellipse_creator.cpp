08b6a4e2dfee2ee351fc6e94c834550d9e01f3fa
danielsf <scott.f.daniel@gmail.com>
2016-10-28 19:49:07 -0700

    brute testwq

--- src/tests/test_ellipse_creator.cpp
+++ src/tests/test_ellipse_creator.cpp
@@ -4,8 +4,8 @@
 
 int main(){
 
-    int dim=4;
-    int n_pts=1000;
+    int dim=5;
+    int n_pts=10000;
     int seed1=99;
     int seed2=342;
 
@@ -62,50 +62,25 @@ int main(){
     }
     printf("center distance %e\n",sqrt(dd));
     
+    for(i=0;i<n_pts;i++){
+        if(ell.contains(pts(i))==0){
+            printf("WARNING does not contain a pt\n");
+            exit(1);
+        }
+    }
 
-    double dot,best_dot,local_best_dot;
-    int best_dex;
-    array_1d<int> chosen,chosen_control;
-    array_1d<int> best_pair, local_best_pair;
-    int pairs=0;
-    while(chosen_control.get_dim()!=dim){
-        best_dot=1.0e30;
-        for(i=0;i<dim;i++){
-            if(chosen_control.contains(i)==0){
-                local_best_dot=1.0e30;
-                for(j=0;j<dim;j++){
-                    if(chosen.contains(j)==0){
-                        dot=0.0;
-                        for(k=0;k<dim;k++){
-                            dot+=bases.get_data(i,k)*ell.bases(j,k);
-                        }
-                        dot=fabs(dot);
-                        if(dot<local_best_dot){
-                            local_best_dot=dot;
-                            local_best_pair.set(0,i);
-                            local_best_pair.set(1,j);
-                        }
-                    }
-                }
-                if(local_best_dot<best_dot){
-                    best_dot=local_best_dot;
-                    best_pair.set(0,local_best_pair.get_data(0));
-                    best_pair.set(1,local_best_pair.get_data(1));
-                }
+
+    double dot;
+    for(i=0;i<dim;i++){
+        for(j=0;j<dim;j++){
+            dot=0.0;
+            for(k=0;k<dim;k++){
+                dot+=bases.get_data(i,k)*ell.bases(j,k);
             }
+            printf("%d %d -- %e -- %e %e\n",
+            i,j,dot,radii.get_data(i),ell.radii(j));
         }
-        chosen_control.add(best_pair.get_data(0));
-        chosen.add(best_pair.get_data(1));
-        dot=0.0;
-        for(i=0;i<dim;i++){
-            dot+=bases.get_data(best_pair.get_data(0),i)*ell.bases(best_pair.get_data(1),i);
-        }
-        printf("%d %d -- %e -- %e %e\n",
-               best_pair.get_data(0),
-               best_pair.get_data(1),
-               dot,
-               radii.get_data(best_pair.get_data(0)),
-               ell.radii(best_pair.get_data(1)));
+        printf("\n");
     }
 
 }
