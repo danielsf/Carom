ddb0cd5b150b99faa6f3f60a85ed4d7d24395d5d
danielsf <scott.f.daniel@gmail.com>
2016-12-17 14:46:16 -0800

    create jellyBean nlive plot

--- scripts/generate_jellyBean_nlive.py
+++ scripts/generate_jellyBean_nlive.py
@@ -19,23 +19,38 @@ plt.figsize = (30,30)
 header_list = []
 label_list= []
 
+xmin = 3.0e30
+xmax = -3.0e30
+ymin = 3.0e30
+ymax = -3.0e30
 
 for i_set, n_live in enumerate(n_live_list):
     print 'plotting ',i_set
     data = None
     for i_fig, dim in enumerate(((6, 9), (0,1))):
         plt.subplot(2,1,i_fig + 1)
-        
+
         file_name = os.path.join(multinest_dir,
                                  "gaussianJellyBean_d12_s99_n%d_t1.00e-03.txt" % n_live)
-        
+
         xx, yy, data = scatter_from_multinest_projection(file_name, 12, dim[0], dim[1],
                                                         data=data)
 
         color = plt.cm.gist_ncar(i_set*50)
         hh = plt.scatter(xx, yy, color=[color]*len(xx), s=7)
-        
+
+        plt.xlabel('$\\theta_%d$' % dim[0], fontsize=15)
+        plt.ylabel('$\\theta_%d$' % dim[1], fontsize=15)
+
         if i_fig != 0:
+            if xx.min()<xmin:
+                xmin = xx.min()
+            if xx.max()>xmax:
+                xmax = xx.max()
+            if yy.min()<ymin:
+                ymin = yy.min()
+            if yy.max()>ymax:
+                ymax = yy.max()
             scatter_name = file_name.replace('.txt', '_carom.sav')
             with open(scatter_name, 'r') as file_handle:
                 lines = file_handle.readlines()
@@ -44,6 +59,12 @@ for i_set, n_live in enumerate(n_live_list):
             label_list.append('n_live %.2e; n_calls %.3e' % (n_live, n_calls))
 
 plt.subplot(2,1,2)
+
+dx = xmax-xmin
+dy = ymax-ymin
+
+plt.xlim((xmin, xmax+0.4*dx))
+plt.ylim((ymin, ymax+0.4*dx))
 plt.legend(header_list, label_list, fontsize=10, loc=0)
 plt.tight_layout()
 plt.savefig(os.path.join(fig_dir, 'jellyBean_nlive_comparison.png'))
