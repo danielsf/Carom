2c582a0653730bd88be32921111f1905c3de0152
danielsf <scott.f.daniel@gmail.com>
2016-12-20 16:30:03 -0800

    add dud multinest to slow lump time series

--- scripts/generate_lump_slow_time_series.py
+++ scripts/generate_lump_slow_time_series.py
@@ -21,7 +21,15 @@ if __name__ == "__main__":
     delta_chi = 21.03
     full_dim = 12
     nlive = 5000
+    n_dud = 300
     seed = 33
+
+    multinest_dud_file = os.path.join(multinest_dir,
+                                      "nonGaussianLump_d12_s99_n%d_t1.00e-03.txt" % n_dud)
+
+    multinest_dud_scatter = os.path.join(multinest_dir,
+                                         "nonGaussianLump_d12_s99_n%d_t1.00e-03_carom.sav" % n_dud)
+
 
     multinest_file = os.path.join(multinest_dir,
                                   "nonGaussianLump_d12_s99_n%d_t1.00e-03.txt" % nlive)
@@ -35,9 +43,15 @@ if __name__ == "__main__":
     n_mult_calls = len(scatter_lines)-1
     del scatter_lines
 
+    with open(multinest_dud_scatter, 'r') as file_handle:
+        scatter_lines = file_handle.readlines()
+    n_mult_dud_calls = len(scatter_lines)-1
+    del scatter_lines
+
     dalex_file = os.path.join(dalex_dir,'lump_d12_s%d_output.sav' % seed)
 
     m_data = None
+    m_d_data = None
     d_data = None
 
     time_list = [75000, 85000, 150000, 200000]
@@ -47,6 +61,9 @@ if __name__ == "__main__":
 
     m_x, m_y, m_data = scatter_from_multinest_projection(multinest_file,
                                                          full_dim, ix, iy, data=m_data)
+
+    m_d_x, m_d_y, m_d_data = scatter_from_multinest_projection(multinest_dud_file,
+                                                               full_dim, ix, iy, data=m_d_data)
 
     xmin = m_x.min()
     xmax = m_x.max()
@@ -61,9 +78,12 @@ if __name__ == "__main__":
 
         plt.subplot(2,2,i_fig+1)
         m_hh = plt.scatter(m_x, m_y, color='k', s=5)
+        m_d_hh = plt.scatter(m_d_x, m_d_y, color='c', s=5)
         #if i_fig == len(time_list)-1:
         #    header_list.append(m_hh)
-        #    label_list.append('MultiNest;\n%.2e $\chi^2$ calls' % n_mult_calls)
+        #    label_list.append('MultiNest; %.2e $\chi^2$ calls' % n_mult_calls)
+        #    header_list.append(m_d_hh)
+        #    label_list.append('MultiNest; %.2e $\chi^2$ calls' % n_mult_dud_calls)
 
         (d_x, d_y, d_min,
          d_target, d_data) = scatter_from_carom(dalex_file, full_dim, ix, iy,
@@ -87,32 +107,37 @@ if __name__ == "__main__":
         if d_y.max()>ymax:
             ymax=d_y.max()
 
-        d_hh_p1 = plt.scatter(d_xp1, d_yp1, color='g', marker='+', s=7)
+        d_hh_p1 = plt.scatter(d_xp1, d_yp1, color='g', marker='+', s=15)
 
         d_hh = plt.scatter(d_x, d_y, color='r', marker='x', s=7)
         if i_fig == len(time_list)-1:
             header_list.append(d_hh_p1)
-            label_list.append('Dale$\chi$ $\chi^2<= 94.16 + 21.03$')
+            label_list.append('Dale$\chi$; $\chi^2<= 94.16 + 21.03$')
             header_list.append(d_hh)
-            label_list.append('Dale$\chi$ $\chi^2<=\chi^2_{min}+21.03$')
+            label_list.append('Dale$\chi$; $\chi^2<=\chi^2_{min}+21.03$')
 
     dx=xmax-xmin
     dy=ymax-ymin
 
-    xmin -= 0.2*dx
-    ymin -= 0.3*dy
-    xmax += 0.1*dy
-    ymax += 0.1*dy
+    xmin -= 0.1*dx
+    ymin -= 0.2*dy
+    xmax += 0.2*dx
+    ymax += 0.2*dy
 
 
     for i_fig, limit in enumerate(time_list):
 
         plt.subplot(2,2,i_fig+1)
 
+        #if i_fig == len(time_list)-1:
+        #    xmin -= 0.4*dx
+        #    ymin -= 0.4*dy
+        #    ymax += 0.2*dy
+
         plt.xlim(xmin, xmax)
         plt.ylim(ymin, ymax)
 
-        plt.text(xmin+0.5*dx, ymax-0.3*dy,
+        plt.text(xmin+0.4*dx, ymax-0.3*dy,
                  'Dale$\chi$ calls to $\chi^2$: %.2e\n' % limit
                  + 'Dale$\chi$ $\chi^2$ min: %.3e' % min_dict[limit],
                  fontsize=10)
