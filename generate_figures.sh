df0e8dc4350c1bf6c685aa015864e3d0968b099d
danielsf <scott.f.daniel@gmail.com>
2016-12-23 11:03:47 -0800

    add bash script to generate all figures

---
+++ generate_figures.sh
@@ -0,0 +1,10 @@
+python scripts/generate_truth_contours.py
+python scripts/generate_likelihood_heat_map.py
+python scripts/generate_lump_nlive.py
+python scripts/generate_jellyBean_nlive.py
+python scripts/generate_jellyBean_time_series.py
+python scripts/generate_lump_time_series.py
+python scripts/generate_slow_jellyBean_time_series.py
+python scripts/generate_lump_slow_time_series.py
+python scripts/generate_jellyBean_histogram.py
+python scripts/generate_jellyeBean_multinest_histogram.py
