691be2b211b08327356fa139075ac8d7439a9a3d
danielsf <scott.f.daniel@gmail.com>
2017-02-15 21:55:26 -0800

    remove perpendicular starburst search

--- src/carom/dalex.cpp
+++ src/carom/dalex.cpp
@@ -1318,71 +1318,7 @@ int dalex::simplex_boundary_search(const int specified, const int i_origin,
         start_path=pt_start;
     }
 
-    base_dir.reset_preserving_room();
-    array_1d<double> trial_dir;
-    base_dir.set_name("dalex_boundary_base_dir");
-    trial_dir.set_name("dalex_boundary_trial_dir");
-    array_2d<double> perp_dir;
-    perp_dir.set_name("dalex_boundary_perp_dir");
-    array_1d<int> good_dexes;
-    good_dexes.set_name("dalex_boundary_good_dexes");
-    int i_midst;
-    int pre_midpt=_chifn->get_pts();
-    double sgn;
-    if(i_next[0]!=specified){
-        for(i=0;i<_chifn->get_dim();i++){
-            base_dir.set(i,_chifn->get_pt(i_next[0],i)-_chifn->get_pt(specified,i));
-        }
-        base_dir.normalize();
-        while(perp_dir.get_rows()<_chifn->get_dim()-1){
-            for(i=0;i<_chifn->get_dim();i++){
-                trial_dir.set(i,normal_deviate(_chifn->get_dice(),0.0,1.0));
-            }
-            component=0.0;
-            for(i=0;i<_chifn->get_dim();i++){
-                component+=trial_dir.get_data(i)*base_dir.get_data(i);
-            }
-            for(i=0;i<_chifn->get_dim();i++){
-                trial_dir.subtract_val(i,component*base_dir.get_data(i));
-            }
-            for(i=0;i<perp_dir.get_rows();i++){
-                component=0.0;
-                for(j=0;j<_chifn->get_dim();j++){
-                    component+=trial_dir.get_data(j)*perp_dir.get_data(i,j);
-                }
-                for(j=0;j<_chifn->get_dim();j++){
-                    trial_dir.subtract_val(j,component*perp_dir.get_data(i,j));
-                }
-            }
-            component=trial_dir.normalize();
-            if(component>1.0e-20){
-                perp_dir.add_row(trial_dir);
-            }
-        }
 
-        for(i=specified;i<i_next[0];i++){
-            if(_chifn->get_fn(i)<target()){
-                good_dexes.add(i);
-            }
-        }
-        i_midst=good_dexes.get_data(good_dexes.get_dim()/2);
-        for(i=0;i<perp_dir.get_rows();i++){
-            for(sgn=-1.0;sgn<1.1;sgn+=2.0){
-                for(j=0;j<_chifn->get_dim();j++){
-                    trial_dir.set(j,sgn*perp_dir.get_data(i,j));
-                }
-                bisection(i_midst,trial_dir,target(),0.01);
-            }
-        }
-        j=0;
-        for(i=pre_midpt;i<_chifn->get_pts();i++){
-            if(_chifn->get_fn(i)<target()){
-                j++;
-            }
-        }
-        printf("bisection sampled %d good %d\n",
-               _chifn->get_pts()-pre_midpt,j);
-    }
 
     for(i=start_path;i<_chifn->get_pts();i++){
         if(_tendril_path.get_rows()==0){
@@ -1630,37 +1566,6 @@ void dalex::tendril_search(int specified){
     }
 
     local_ellipse.build(exclusion_points);
-
-    int old_type = _chifn->get_search_type();
-    _chifn->set_search_type(_type_tendril_fill);
-
-    int n_fill = (_chifn->get_pts()-pt_0)/10;
-    int n_fill_good = 0;
-    array_1d<double> trial,ell_pt;
-    trial.set_name("dalex_tendril_trial");
-    ell_pt.set_name("dalex_tendril_ell_pt");
-    if(_ellipse_sampler.is_initialized()==0){
-        _ellipse_sampler.initialize(_chifn->get_dim(), _chifn->random_int());
-    }
-
-    for(i=0;i<n_fill;i++){
-        _ellipse_sampler.get_pt(ell_pt);
-        for(j=0;j<_chifn->get_dim();j++){
-            trial.set(j,local_ellipse.center(j));
-        }
-        for(j=0;j<_chifn->get_dim();j++){
-            for(k=0;k<_chifn->get_dim();k++){
-                trial.add_val(k,ell_pt.get_data(j)*local_ellipse.radii(j)*local_ellipse.bases(j,k));
-            }
-        }
-        evaluate(trial,&mu,&j);
-        if(mu<target()){
-            n_fill_good++;
-        }
-    }
-    printf("    n_fill %d good %d\n",n_fill,n_fill_good);
-
-    _chifn->set_search_type(old_type);
 
     _exclusion_zones.add(local_ellipse);
     printf("\n    strike out (%d strikes; %d pts)\n",
