2f164d77cc51415842b7c760f3c113fccf57a5fc
danielsf <scott.f.daniel@gmail.com>
2017-07-11 18:46:50 -0400

    class based hypberbox integratorwq

--- src/examples/dalex_bayes_hyperbox_12.cpp
+++ src/examples/dalex_bayes_hyperbox_12.cpp
@@ -27,31 +27,260 @@ class hyperbox_integrator{
 
     public:
 
+        hyperbox_list hb_list;
+
         hyperbox_integrator(){
             _dalex_pts.set_name("integrator_dalex_pts");
             _dalex_chisq.set_name("integrator_dalex_chisq");
+            _dx.set_name("integrator_dx");
+            _pixel_list.set_name("integrator_pixel_list");
+            _pixel_mapping.set_name("integrator_pixel_mapping");
+            _local_pixel.set_name("integrator_local_pixel");
         }
 
         ~hyperbox_integrator(){}
 
-        void set_pts(array_2d<double> &pts, array_1d<double> &xx){
+        int get_n_pts(){
+            int sum=0;
+            int i;
+            for(i=0;i<_pixel_mapping.get_rows();i++){
+                sum+=_pixel_mapping.get_cols(i);
+            }
+            if(sum!=_dalex_pts.get_rows()){
+                printf("WARNING _dalex_pts %d; _pixel_mapping %d\n",
+                _dalex_pts.get_rows(),sum);
+                exit(1);
+            }
+            return _dalex_pts.get_rows();
+        }
+
+        double chisq_min(){
+            return _chisq_min;
+        }
+
+        void _add_pixel(int pt_dex){
+            pixellate(_dalex_pts(pt_dex),_dx,_local_pixel);
+            int i,j;
+            int pixel_dex=-1;
+            for(i=0;i<_pixel_list.get_rows();i++){
+                pixel_dex=i;
+                for(j=0;j<_dalex_pts.get_cols();j++){
+                    if(_pixel_list.get_data(i,j)!=_local_pixel.get_data(j)){
+                        pixel_dex=-1;
+                        break;
+                    }
+                }
+                if(pixel_dex>=0){
+                    break;
+                }
+            }
+
+            int already_contained=0;
+            if(pixel_dex<0){
+                _pixel_list.add_row(_local_pixel);
+                _pixel_mapping.set(_pixel_list.get_rows()-1,0,pt_dex);
+                pixel_dex=_pixel_list.get_rows()-1;
+            }
+            else{
+                already_contained=0;
+                for(i=0;i<_pixel_mapping.get_cols(i);i++){
+                    if(_pixel_mapping.get_data(pixel_dex,i)==pt_dex){
+                        already_contained=1;
+                        break;
+                    }
+                }
+                if(already_contained==0){
+                    _pixel_mapping.add(pixel_dex,pt_dex);
+                }
+            }
+
+            double xmin,xmax;
+            for(i=0;i<_dalex_pts.get_cols();i++){
+                xmin=_pixel_list.get_data(pixel_dex,i)*_dx.get_data(i)-0.5*_dx.get_data(i);
+                xmax=_pixel_list.get_data(pixel_dex,i)*_dx.get_data(i)+0.5*_dx.get_data(i);
+                if(_dalex_pts.get_data(pt_dex,i)<xmin){
+                    printf("pixelization failed on min: %e !> %e\n",
+                    _dalex_pts.get_data(pt_dex,i),xmin);
+                    exit(1);
+                }
+                if(_dalex_pts.get_data(pt_dex,i)>xmax){
+                    printf("pixelization failed on max: %e !< %e\n",
+                    _dalex_pts.get_data(pt_dex,i),xmax);
+                    exit(1);
+                }
+            }
+
+        }
+
+        void set_pts(array_2d<double> &pts, array_1d<double> &xx, double delta_chisq){
              _dalex_pts.reset_preserving_room();
              _dalex_chisq.reset_preserving_room();
+             _dx.reset_preserving_room();
+             _pixel_list.reset_preserving_room();
+             _pixel_mapping.reset_preserving_room();
              int i;
+             _chisq_min=2.0*exception_value;
              for(i=0;i<pts.get_rows();i++){
                  _dalex_pts.add_row(pts(i));
                  _dalex_chisq.add(xx.get_data(i));
+                 if(i==0 || xx.get_data(i)<_chisq_min){
+                     _chisq_min=xx.get_data(i);
+                 }
              }
+
+             int j;
+             array_1d<double> good_xmin, good_xmax;
+             good_xmin.set_name("integrator_set_pts_good_xmin");
+             good_xmax.set_name("integrator_set_pts_good_xmax");
+             for(i=0;i<_dalex_pts.get_rows();i++){
+                 if(_dalex_chisq.get_data(i)<=_chisq_min+delta_chisq){
+                     for(j=0;j<_dalex_pts.get_cols();j++){
+                         if(j>=good_xmin.get_dim() || _dalex_pts.get_data(i,j)<good_xmin.get_data(j)){
+                             good_xmin.set(j,_dalex_pts.get_data(i,j));
+                         }
+                         if(j>=good_xmax.get_dim() || _dalex_pts.get_data(i,j)>good_xmax.get_data(j)){
+                             good_xmax.set(j,_dalex_pts.get_data(i,j));
+                         }
+                     }
+                 }
+             }
+
+             for(i=0;i<_dalex_pts.get_cols();i++){
+                 _dx.set(i,0.1*(good_xmax.get_data(i)-good_xmin.get_data(i)));
+             }
+
+             for(i=0;i<_dalex_pts.get_rows();i++){
+                 _add_pixel(i);
+             }
+             get_n_pts();
+             printf("done with initialization\n");
         }
 
         void add_pt(array_1d<double> &pt, double xx){
             _dalex_pts.add_row(pt);
             _dalex_chisq.add(xx);
+            if(_dalex_pts.get_rows()!=_dalex_chisq.get_dim()){
+                printf("WARNING array points %d chisq values %d\n",
+                _dalex_pts.get_rows(),_dalex_chisq.get_dim());
+                exit(1);
+            }
+            _add_pixel(_dalex_pts.get_rows()-1);
+        }
+
+        void create_hyperboxes(){
+            hb_list.reset();
+            hb_list.set_room(_dalex_pts.get_rows());
+            int n_box_pts=0;
+            int pt_dex;
+            double t_pre_init=double(time(NULL));
+            array_1d<double> pt;
+            pt.set_name("integrator_create_hyperboxes_pt");
+            array_2d<double> box_pts;
+            box_pts.set_name("integrator_create_hyperboxes_box_pts");
+            array_1d<double> box_min,box_max;
+            box_min.set_name("integrator_create_hyperboxes_box_min");
+            box_max.set_name("integrator_create_hyperboxes_box_max");
+            pt.reset_preserving_room();
+            hyperbox hb;
+            int i,j,k;
+            int dim;
+            dim=_dalex_pts.get_cols();
+            for(i=0;i<_pixel_list.get_rows();i++){
+                box_pts.reset_preserving_room();
+                for(j=0;j<dim;j++){
+                    box_min.set(j,_pixel_list.get_data(i,j)*_dx.get_data(j)-0.5*_dx.get_data(j));
+                    box_max.set(j,_pixel_list.get_data(i,j)*_dx.get_data(j)+0.5*_dx.get_data(j));
+                    if(box_min.get_data(j)>box_max.get_data(j)){
+                       printf("setting min/max backwards\n");
+                       printf("%e  %e\n",box_min.get_data(j),box_max.get_data(j));
+                       printf("%d %e\n",_pixel_list.get_data(i,j),_dx.get_data(j));
+                       exit(1);
+                    }
+                }
+                for(j=0;j<_pixel_mapping.get_cols(i);j++){
+                    pt_dex = _pixel_mapping.get_data(i,j);
+                    for(k=0;k<dim;k++){
+                        pt.set(k,_dalex_pts.get_data(pt_dex,k));
+                        if(pt.get_data(k)>box_max.get_data(k) || pt.get_data(k)<box_min.get_data(k)){
+                            printf("assigning point that is outside of box\n");
+                            printf("failed: %e < %e < %e\n",
+                            box_min.get_data(k),pt.get_data(k),box_max.get_data(k));
+                            exit(1);
+                        }
+                    }
+                    pt.set(dim,_dalex_chisq.get_data(pt_dex)-_chisq_min);
+                    box_pts.add_row(pt);
+                }
+                if(box_pts.get_rows()==0){
+                    printf("trying to initialize empty box\n");
+                    exit(1);
+                }
+                for(j=0;j<box_pts.get_rows();j++){
+                    for(k=0;k<dim;k++){
+                        if(box_pts.get_data(j,k)<box_min.get_data(k) ||
+                           box_pts.get_data(j,k)>box_max.get_data(k)){
+
+                            printf("hyper box assignment failed\n");
+                            printf("%e %e %e\n",box_min.get_data(k),
+                            box_pts.get_data(j,k),box_max.get_data(k));
+                            exit(1);
+                        }
+                    }
+                }
+                n_box_pts+=box_pts.get_rows();
+                hb.build(box_pts,box_min,box_max);
+                hb_list.add(hb);
+            }
+
+            printf("hyperboxes %d; points %d; array_pts %d\n",hb_list.ct(),n_box_pts,get_n_pts());
+
+            for(i=0;i<hb_list.ct();i++){
+                if(hb_list(i)->dim()!=dim){
+                    printf("WARNING initial box has dim %d\n",
+                    hb_list(i)->dim());
+                    exit(1);
+                 }
+            }
+
+            int all_clear=0;
+            array_1d<double> min1,min2,max1,max2;
+            array_2d<double> pts1,pts2;
+            min1.set_name("min1");
+            min2.set_name("min2");
+            max1.set_name("max1");
+            max2.set_name("max2");
+            pts1.set_name("pts1");
+            pts2.set_name("pts2");
+            double t_pre_split=double(time(NULL));
+            while(all_clear==0){
+                all_clear=1;
+                for(i=0;i<hb_list.ct();i++){
+                    if(hb_list(i)->n_pts()>1){
+                        all_clear=0;
+                        hb_list(i)->split(pts1,min1,max1,pts2,min2,max2);
+                        hb_list(i)->build(pts1,min1,max1);
+                        hb.build(pts2,min2,max2);
+                        hb_list.add(hb);
+                    }
+                }
+            }
+
+            for(i=0;i<hb_list.ct();i++){
+                if(hb_list(i)->n_pts()!=1){
+                    printf("have box with %d pts\n",hb_list(i)->n_pts());
+                    exit(1);
+                }
+            }
         }
 
     private:
         array_2d<double> _dalex_pts;
-        array_1d<double> _dalex_chisq;
+        array_1d<double> _dalex_chisq,_dx;
+        array_2d<int> _pixel_list;
+        asymm_array_2d<int> _pixel_mapping;
+        double _chisq_min;
+        array_1d<int> _local_pixel;
+
 };
 
 void get_hyperbox_list(array_2d<double> &dalex_pts,
@@ -319,17 +548,15 @@ int main(int iargc, char *argv[]){
 
     array_1d<double> pt;
     pt.set_name("pt");
-    array_1d<double> dalex_chisq;
+    array_1d<double> dalex_chisq,xmin,xmax;
     array_2d<double> dalex_pts;
     double xx;
     dalex_chisq.set_name("dalex_chisq");
     dalex_pts.set_name("dalex_pts");
-
-    printf("n_cols %d\n",n_cols);
-    double chisq_min=2.0*exception_value;
-    array_1d<double> xmin,xmax;
     xmin.set_name("xmin");
     xmax.set_name("xmax");
+
+    printf("n_cols %d\n",n_cols);
     while(fscanf(in_file,"%le",&xx)>0){
         pt.set(0,xx);
         if(xmin.get_dim()==0 || xx<xmin.get_data(0)){
@@ -351,9 +578,6 @@ int main(int iargc, char *argv[]){
         dalex_pts.add_row(pt);
         fscanf(in_file,"%le",&xx);
         dalex_chisq.add(xx);
-        if(xx<chisq_min){
-            chisq_min=xx;
-        }
         for(i=dim+1;i<n_cols;i++){
             fscanf(in_file,"%le",&xx);
         }
@@ -363,7 +587,6 @@ int main(int iargc, char *argv[]){
     printf("read in data\n");
 
     kd_tree dalex_tree(dalex_pts,xmin,xmax);
-    hyperbox_list hb_list;
 
     jellyBeanData *chifn;
 
@@ -416,6 +639,12 @@ int main(int iargc, char *argv[]){
 
     double t_build_hyperbox=0.0;
     double t0;
+
+    hyperbox_integrator hb_integrator;
+    hb_integrator.set_pts(dalex_pts, dalex_chisq, delta_chisq);
+    dalex_pts.reset();
+    dalex_chisq.reset();
+
     while(total_pts_added<n_new_pts){
         ln_posterior.reset_preserving_room();
         ln_vol_arr.reset_preserving_room();
@@ -424,30 +653,29 @@ int main(int iargc, char *argv[]){
         sorted_chisq.reset_preserving_room();
         chisq_dex.reset_preserving_room();
         posterior_chisq.reset_preserving_room();
-        hb_list.reset();
 
         t0=double(time(NULL));
-        get_hyperbox_list(dalex_pts, dalex_chisq, delta_chisq, hb_list);
+        hb_integrator.create_hyperboxes();
         t_build_hyperbox+=double(time(NULL))-t0;
 
         posterior_chisq.reset_preserving_room();
-        for(i=0;i<hb_list.ct();i++){
-            posterior_chisq.set(i,hb_list(i)->pts(0,dim));
+        for(i=0;i<hb_integrator.hb_list.ct();i++){
+            posterior_chisq.set(i,hb_integrator.hb_list(i)->pts(0,dim));
         }
         ln_posterior.set_name("ln_posterior");
         ln_vol_arr.set_name("ln_vol_arr");
-        for(i=0;i<hb_list.ct();i++){
+        for(i=0;i<hb_integrator.hb_list.ct();i++){
             ln_vol=0.0;
             for(j=0;j<dim;j++){
-                if(hb_list(i)->max(j)-hb_list(i)->min(j)>1.0e-120){
-                    ln_vol+=log(hb_list(i)->max(j)-hb_list(i)->min(j));
+                if(hb_integrator.hb_list(i)->max(j)-hb_integrator.hb_list(i)->min(j)>1.0e-120){
+                    ln_vol+=log(hb_integrator.hb_list(i)->max(j)-hb_integrator.hb_list(i)->min(j));
                 }
                 else{
                     ln_vol=-69.0*dim;
                     break;
                 }
             }
-            ln_posterior.set(i,-0.5*hb_list(i)->pts(0,dim)+ln_vol);
+            ln_posterior.set(i,-0.5*hb_integrator.hb_list(i)->pts(0,dim)+ln_vol);
             ln_vol_arr.set(i,ln_vol);
         }
 
@@ -493,9 +721,9 @@ int main(int iargc, char *argv[]){
         }
 
         sort(valid_vol,valid_vol_sorted,valid_vol_dex);
-        printf("max vol %e -- max chisq %e\n",
+        printf("max vol %e -- max chisq %e -- min chisq %e\n",
         valid_vol_sorted.get_data(valid_vol_dex.get_dim()-1),
-        max_valid_chisq);
+        max_valid_chisq,hb_integrator.chisq_min());
         j=0;
         xx=valid_vol_sorted.get_data(valid_vol_dex.get_dim()-1);
         for(i=0;i<valid_vol_sorted.get_dim();i++){
@@ -517,18 +745,18 @@ int main(int iargc, char *argv[]){
                 for(i=0;i<dim;i++){
                     for(sgn=-1.0;sgn<2.0;sgn+=2.0){
                         for(j=0;j<dim;j++){
-                            pt.set(j,0.5*(hb_list(dex)->max(j)+
-                                          hb_list(dex)->min(j)));
+                            pt.set(j,0.5*(hb_integrator.hb_list(dex)->max(j)+
+                                          hb_integrator.hb_list(dex)->min(j)));
                         }
-                        pt.add_val(i,sgn*factor*(hb_list(dex)->max(i)-
-                                                 hb_list(dex)->min(i)));
+                        pt.add_val(i,sgn*factor*(hb_integrator.hb_list(dex)->max(i)-
+                                                 hb_integrator.hb_list(dex)->min(i)));
 
 
                         dalex_tree.nn_srch(pt,1,neigh,dist);
                         if(dist.get_data(0)>1.0e-20){
-                            dalex_pts.add_row(pt);
+                            xx = chifn[0](pt);
+                            hb_integrator.add_pt(pt, xx);
                             dalex_tree.add(pt);
-                            dalex_chisq.add(chifn[0](pt));
                             pts_added++;
                          }
                     }
@@ -545,9 +773,9 @@ int main(int iargc, char *argv[]){
             printf("did not add any points\n");
             exit(1);
         }
-        if(dalex_tree.get_pts()!=dalex_pts.get_rows()){
+        if(dalex_tree.get_pts()!=hb_integrator.get_n_pts()){
             printf("tree and array have different n %d %d\n",
-            dalex_tree.get_pts(),dalex_pts.get_rows());
+            dalex_tree.get_pts(),hb_integrator.get_n_pts());
             exit(1);
         }
         total_pts_added+=pts_added;
@@ -572,10 +800,10 @@ int main(int iargc, char *argv[]){
         local_prob+=exp(ln_posterior.get_data(dex));
         fprintf(out_file,"%e %e %e ",
                 local_prob/total_prob,
-                posterior_chisq.get_data(dex)+chisq_min,
+                posterior_chisq.get_data(dex)+hb_integrator.chisq_min(),
                 ln_vol_arr.get_data(dex));
         for(j=0;j<dim;j++){
-            fprintf(out_file,"%e %e ",hb_list(dex)->min(j),hb_list(dex)->max(j));
+            fprintf(out_file,"%e %e ",hb_integrator.hb_list(dex)->min(j),hb_integrator.hb_list(dex)->max(j));
         }
         fprintf(out_file,"\n");
     }
