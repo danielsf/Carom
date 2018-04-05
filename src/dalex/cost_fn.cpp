918c0fdfa6cf7253ef9e910c28dcc361755579cd
danielsf <scott.f.daniel@gmail.com>
2018-04-05 14:41:35 -0700

    print statement

--- src/dalex/cost_fn.cpp
+++ src/dalex/cost_fn.cpp
@@ -110,8 +110,13 @@ void cost_fn::_set_scalar_norm(){
     double median = norm_sorted.get_data(norm_dex.get_dim()/2);
     _scalar_norm = 5.0;
     printf("    set scalar norm to %e\n",_scalar_norm);
+
+    printf("    min %e max %e\n",
+           norm_sorted.get_data(0),
+           norm_sorted.get_data(norm_sorted.get_dim()-1));
+
     printf("    median %e geometric %e harmonic %e\n",
-    median,geometric_mean,harmonic_mean);
+           median,geometric_mean,harmonic_mean);
 
     for(idim=0;idim<_relative_norm.get_dim();idim++){
         if(_relative_norm.get_data(idim)>_scalar_norm){
