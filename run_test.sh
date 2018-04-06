e6e6f9d7781f893c511b348accbbe277f46dd699
danielsf <scott.f.daniel@gmail.com>
2018-04-06 14:54:52 -0700

    add script to run tests

---
+++ run_test.sh
@@ -0,0 +1,13 @@
+make curved_12d
+
+out_dir=$1
+
+echo $out_dir
+
+for ss in 771 11 831 452 642 53 19 52 112 90
+do
+    ./bin/curved_12d -s ${ss} -n 600000 \
+    -o ${out_dir}/output_s${ss}.txt \
+    -t ${out_dir}/timing_s${ss}.txt >& ${out_dir}/stdout_${ss}.txt
+
+done
