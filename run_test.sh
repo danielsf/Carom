3b2ce2232a458d40b112a77aa4fffaf3173c7101
danielsf <scott.f.daniel@gmail.com>
2018-04-06 14:55:18 -0700

    run tests in parallel

--- run_test.sh
+++ run_test.sh
@@ -6,8 +6,8 @@ echo $out_dir
 
 for ss in 771 11 831 452 642 53 19 52 112 90
 do
-    ./bin/curved_12d -s ${ss} -n 600000 \
+    nohup nice ./bin/curved_12d -s ${ss} -n 600000 \
     -o ${out_dir}/output_s${ss}.txt \
-    -t ${out_dir}/timing_s${ss}.txt >& ${out_dir}/stdout_${ss}.txt
+    -t ${out_dir}/timing_s${ss}.txt >& ${out_dir}/stdout_${ss}.txt &
 
 done
