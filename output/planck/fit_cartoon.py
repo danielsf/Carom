051512bdfd3a1fb07176aa08557db2271deda22a
danielsf <scott.f.daniel@gmail.com>
2018-06-08 11:51:54 -0700

    play with sigma_sq

--- output/planck/fit_cartoon.py
+++ output/planck/fit_cartoon.py
@@ -84,8 +84,13 @@ if __name__ == "__main__":
 
     #i_matrix = i_dim*order + i_order
 
+    delta_chisq=57.41
     chisq_min = min(training_chisq.min(), validation_chisq.min())
     sigma_sq = (1.0+(training_chisq-chisq_min)/20.0)**2
+
+    sigma_sq = np.where(training_chisq<chisq_min+delta_chisq,
+                        1.0,
+                        ((training_chisq-chisq_min)/10.0)**2)
 
     print('starting matrix loop')
     t_start = time.time()
